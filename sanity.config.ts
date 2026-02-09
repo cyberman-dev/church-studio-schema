6b99d4e1fdb0488469d08e3c9e030f296746ee5e
mycyber <106356406+cyberman-dev@users.noreply.github.com>
2026-02-09 12:20:33 -0500

    Add Sanity configuration for Church Events Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,1 +1,13 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas/schema'
+
+export default defineConfig({
+  name: 'church-events',
+  title: 'Church Events Studio',
+  projectId: 's8bpjbaw',
+  dataset: 'production',
+  plugins: [deskTool()],
+  schema: {types: schemaTypes},
+})
 
